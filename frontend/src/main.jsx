5922f77b228369166fa531fa2ae03eca92ec5e98
siascone <spencer.iascone@gmail.com>
2024-10-31 22:45:37 -0600

    add csrf restoration for frontend api fetching

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,19 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store/store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== 'production') {
     window.store = store;
+    window.csrfFetch = csrfFetch;
+    window.restoreCSRF = restoreCSRF;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Provider store={store}>
         <App />
     </Provider>
-  </React.StrictMode>
 );
+
